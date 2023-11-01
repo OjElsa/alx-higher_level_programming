a8b01039205a99261c3caeed0a578845440669f1
OjElsa <elsahojwang06@gmail.com>
2023-11-01 13:21:31 -0700

    prints numbers from 0 to 99 separated by , followed by a space

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 
 for number in range(100):
-    print("{:02d}".format(number), end=", " if number < 99 else "\n", end="")
+    print("{:02d}".format(number), end=", " if number < 99 else "\n")
