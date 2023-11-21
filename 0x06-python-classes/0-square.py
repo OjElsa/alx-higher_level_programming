fc866c49caa51d5677a547d5ffdd9437638c28b1
OjElsa <elsahojwang06@gmail.com>
2023-11-21 14:42:45 -0800

    an empty class Square that defines a square:

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+"""Square modele
+
+This module defines the Square class
+
+"""
+
+
+class Square:
+    """Class Square
+
+    A class representing a square
+
+    Atrributes:
+        None
+
+    """
+
+    pass
