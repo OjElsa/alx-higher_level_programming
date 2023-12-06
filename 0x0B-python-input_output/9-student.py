96017ff8657fd1ed1153abb7ba46018d58ca111d
OjElsa <elsahojwang06@gmail.com>
2023-12-06 13:09:54 -0800

    code refactor

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -4,18 +4,20 @@
 
 
 class Student:
-    """A class that defines arrtributes of a student"""
+    """A class that represents arrtributes of a student"""
 
+    def __init__(self, first_name, last_name, age):
+        """Attributes innsatances of a student
 
-def __init__(self, first_name, last_name, age):
-    """Attributes innsatances of a student
+            Args:
+                first_name(str): the first name of the student
+                last_name(str):the last name of the student
+                age(int): Age of the student
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
 
-    Args:
-        first_name(str): the first name of the student
-        last_name(str):the last name of the student
-        age(int): Age of the student
-
-    """
-def to_json(self):
-    """retrieves a dictionary representation of a Student instance"""
-    return self.__dict__
+    def to_json(self):
+        """retrieves a dictionary representation of a Student instance"""
+        return self.__dict__
