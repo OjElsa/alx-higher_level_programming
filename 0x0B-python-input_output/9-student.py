855ff963222ca21e04a83f8335cf25eb21f57ff9
OjElsa <elsahojwang06@gmail.com>
2023-12-06 12:52:01 -0800

    a class Student that defines a student by  given attributes

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+"""A class Student that defines a student by"""
+
+
+class Student:
+    """A class that defines arrtributes of a student"""
+
+
+def __init__(self, first_name, last_name, age):
+    """Attributes innsatances of a student
+
+    Args:
+        first_name(str): the first name of the student
+        last_name(str):the last name of the student
+        age(int): Age of the student
+
+    """
+def to_json(self):
+    """retrieves a dictionary representation of a Student instance"""
+    return self.__dict__
