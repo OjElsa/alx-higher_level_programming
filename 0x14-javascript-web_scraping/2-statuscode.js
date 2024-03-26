6594efc9132c640109ae65ee086b99b8285facec
OjElsa <elsahojwang06@gmail.com>
2024-03-26 04:56:56 -0700

    script that display the status code of a GET request.

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const url = process.argv[2];
+
+if (!url) {
+  console.error('Usage: ./2-statuscode.js <URL>');
+  process.exit(1);
+}
+
+request.get(url, (error, response) => {
+  if (error) {
+    console.error(error);
+    process.exit(1);
+  }
+  console.log(`code: ${response.statusCode}`);
+});
